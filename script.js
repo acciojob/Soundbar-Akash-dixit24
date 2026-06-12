9c65fec6cd439ff1228fc9b8192234237ba67427
Akash Dixit <137980846+Akash-dixit24@users.noreply.github.com>
2026-06-12 11:53:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const sounds = [
+    "applause",
+    "boo",
+    "gasp",
+    "tada",
+    "victory",
+    "wrong"
+];
+
+let currentAudio = null;
+
+function stopSound() {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+}
+
+document.querySelectorAll(".btn").forEach(button => {
+    button.addEventListener("click", () => {
+        const sound = button.dataset.sound;
+
+        stopSound();
+
+        currentAudio = new Audio(`sounds/${sound}.mp3`);
+        currentAudio.play();
+    });
+});
+
+document.querySelector(".stop").addEventListener("click", () => {
+    stopSound();
+});
